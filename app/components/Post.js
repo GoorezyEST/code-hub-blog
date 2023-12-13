5d47789dcd9ca16f2f706d718e65137065b72a26
GoorezyEST <goorezyestudios@gmail.com>
2023-12-13 20:16:42 -0300

    Created the post component

---
+++ app/components/Post.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Markdown from "markdown-to-jsx";
+import getPostContent from "@/functions/getPostContent";
+
+import styles from "@/styles/modules/post.module.css";
+import Text from "./posts/Text";
+import Subtitle from "./posts/Subtitle";
+import Code from "@/app/components/posts/Code";
+import TextAndImage from "./posts/TextAndImage";
+
+const postComponents = {
+  subtitle: {
+    component: Subtitle,
+  },
+  text: {
+    component: Text,
+  },
+  code: {
+    component: Code,
+  },
+  text_and_image: {
+    component: TextAndImage,
+  },
+};
+
+function PostComponent({ postName }) {
+  //We receive the post name from the props, and we use it
+  //to retrieve all the post content
+  const postData = getPostContent(postName);
+
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.post_title}>{postData.data.title}</h1>
+      <Markdown
+        className={styles.post_content}
+        options={{
+          overrides: postComponents,
+        }}
+      >
+        {postData.content}
+      </Markdown>
+    </section>
+  );
+}
+
+export default PostComponent;
