22e5fa912aeebdb5b419bebc0a8f93b3e3d43078
GoorezyEST <goorezyestudios@gmail.com>
2023-12-14 17:54:27 -0300

    Home page with new sections and styling

--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import React from "react";
 import styles from "@/styles/modules/home.module.css";
+import PostPreview from "./components/PostPreview";
+import getLast6Posts from "@/functions/getLast6Posts";
+import Banner from "./components/Banner";
 
 function Home() {
+  //We get all the posts
+  let postMetadata = getLast6Posts();
+
+  //We create a PostPreview component for each post
+  const postsPreviews = postMetadata.map((post) => {
+    return <PostPreview key={post.slug} postData={post} />;
+  });
+
   return (
-    <section>
-      <h1>Hello</h1>
+    <section className={styles.container}>
+      <Banner text={"CODE"} span={"HUB"} />
+
+      <section className={styles.recent_posts}>
+        <div className={styles.title}>
+          <p>Explora mis posts</p>
+          <h2>Más recientes:</h2>
+        </div>
+        <div className={styles.posts_grid}>{postsPreviews}</div>
+      </section>
     </section>
   );
 }
